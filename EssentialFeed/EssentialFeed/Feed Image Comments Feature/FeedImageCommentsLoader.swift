d4f6ee52bfc221726f48fa73bd5609538cae915a
Oliver Perez <oliverperez.e@gmail.com>
2021-03-17 22:51:37 -0600

    Create the `FeedImageCommentsLoader` protocol boundary.

---
+++ EssentialFeed/EssentialFeed/Feed Image Comments Feature/FeedImageCommentsLoader.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCommentsLoader.swift
+//  EssentialFeed
+//
+//  Created by Oliver Jordy Pérez Escamilla on 17/03/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+protocol FeedImageCommentsLoader {
+	typealias Result = Swift.Result<[FeedImageComment], Error>
+	
+	func load(completion: @escaping (Result) -> Void)
+}
