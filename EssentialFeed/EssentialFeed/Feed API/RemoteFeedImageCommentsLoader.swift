4b6a7fbf1070ae493451b67c21e326777078fcca
Oliver Perez <oliverperez.e@gmail.com>
2021-03-21 21:40:42 -0600

    Remove default value for completion block (move it to the test)

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageCommentsLoader.swift
@@ -22,7 +22,7 @@ public final class RemoteFeedImageCommentsLoader {
 		self.client = client
 	}
 	
-	public func load(completion: @escaping (Error) -> Void = { _ in }) {
+	public func load(completion: @escaping (Error) -> Void) {
 		client.get(from: url) { error in
 			completion(.connectivity)
 		}
