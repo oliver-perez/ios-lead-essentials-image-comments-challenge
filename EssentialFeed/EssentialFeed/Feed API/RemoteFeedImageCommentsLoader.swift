410b378277699fa53cab633d5019fb4bb73d6ef5
Oliver Perez <oliverperez.e@gmail.com>
2021-03-25 22:40:23 -0600

    Move mapping `RemoteFeedImageComments` logic into a new type `RemoteFeedImageCommentMapper`

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageCommentsLoader.swift
@@ -30,8 +30,8 @@ public final class RemoteFeedImageCommentsLoader {
 
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.items.map { $0.dto }))
+				if let items = try? RemoteFeedImageCommentMapper.map(data, response) {
+					completion(.success(items))
 				} else {
 					completion(.failure(.invalidData))
 				}
@@ -42,50 +42,60 @@ public final class RemoteFeedImageCommentsLoader {
 	
 }
 
-private struct Root: Decodable {
-	let items: [RemoteFeedImageComment]
-}
-
-private struct RemoteFeedImageComment {
-	let id: UUID
-	let message:	String
-	let creationDate: String
-	let author: RemoteImageCommentAuthor
+final private class RemoteFeedImageCommentMapper {
 	
-	init(id: UUID,
-			 message: String,
-			 creationDate: String,
-			 author: RemoteImageCommentAuthor) {
-		self.id = id
-		self.message = message
-		self.creationDate = creationDate
-		self.author = author
+	private struct Root: Decodable {
+		let items: [RemoteFeedImageComment]
 	}
-}
 
-extension RemoteFeedImageComment {
-	var dto: FeedImageComment  {
-		.init(id: id,
-					message: message,
-					creationDate: creationDate,
-					author: .init(username: author.username))
+	private struct RemoteFeedImageComment: Decodable {
+		let id: UUID
+		let message:	String
+		let creationDate: String
+		let author: RemoteImageCommentAuthor
+		
+		var dto: FeedImageComment  {
+			.init(id: id,
+						message: message,
+						creationDate: creationDate,
+						author: .init(username: author.username))
+		}
+		
+		init(id: UUID,
+				 message: String,
+				 creationDate: String,
+				 author: RemoteImageCommentAuthor) {
+			self.id = id
+			self.message = message
+			self.creationDate = creationDate
+			self.author = author
+		}
+		
+			private enum CodingKeys: String, CodingKey {
+				case id
+				case message
+				case creationDate = "created_at"
+				case author
+			}
+		
 	}
-}
-
-extension RemoteFeedImageComment: Decodable {
-	private enum CodingKeys: String, CodingKey {
-		case id
-		case message
-		case creationDate = "created_at"
-		case author
-	}
-}
-
-
-private struct RemoteImageCommentAuthor: Decodable {
-	public let username: String
 	
-	public init(username: String) {
-		self.username = username
+	private struct RemoteImageCommentAuthor: Decodable {
+		public let username: String
+		
+		public init(username: String) {
+			self.username = username
+		}
+	}
+	
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImageComment] {
+		guard response.statusCode == 200 else {
+			throw RemoteFeedImageCommentsLoader.Error.invalidData
+		}
+		
+		return try JSONDecoder()
+			.decode(Root.self, from: data)
+			.items
+			.map(\.dto)
 	}
 }
