cd5f186be8a39616450e452bc900f146900f41b0
Oliver Perez <oliverperez.e@gmail.com>
2021-03-30 22:25:01 -0600

    Loads feed image comments on `viewDidLoad`

--- EssentialFeed/EssentialFeediOSTests/FeedImageCommentsViewController.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedImageCommentsViewController.swift
@@ -8,22 +8,45 @@
 
 import XCTest
 
-final class FeedImageCommentsViewController {
-	init(loader: FeedImageCommentsViewControllerTests.LoaderSpy) {
+final class FeedImageCommentsViewController: UIViewController {
+	
+	private var loader: FeedImageCommentsViewControllerTests.LoaderSpy?
+	
+	convenience init(loader: FeedImageCommentsViewControllerTests.LoaderSpy) {
+		self.init()
+		self.loader = loader
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		loader?.load()
 	}
 }
 
 class FeedImageCommentsViewControllerTests: XCTestCase {
 	
-	func test_init_doesNotLoadFeed() {
+	func test_init_doesNotLoadFeedImageComments() {
 		let loader = LoaderSpy()
 		_ = FeedImageCommentsViewController(loader: loader)
 		
 		XCTAssertEqual(loader.loadCount, 0)
 	}
 	
+	func test_viewDidLoad_loadsFeedImageComments() {
+		let loader = LoaderSpy()
+		let sut = FeedImageCommentsViewController(loader: loader)
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(loader.loadCount, 1)
+	}
+	
 	final class LoaderSpy {
 		private(set) var loadCount = 0
+		
+		func load() {
+			loadCount += 1
+		}
 	}
 	
 }
