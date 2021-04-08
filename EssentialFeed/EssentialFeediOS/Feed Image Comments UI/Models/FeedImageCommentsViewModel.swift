b773e651b39b2c812e2a941b3ff4bb0b4b39976a
Oliver Perez <oliverperez.e@gmail.com>
2021-04-07 19:15:45 -0500

    Remove mutable state from `FeedImageCommentsViewModel`, the `FeedImageCommentsViewModel` only needs to forward state changes, so state is only transient.

--- EssentialFeed/EssentialFeediOS/Feed Image Comments UI/Models/FeedImageCommentsViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed Image Comments UI/Models/FeedImageCommentsViewModel.swift
@@ -9,49 +9,32 @@
 import EssentialFeed
 
 final class FeedImageCommentsViewModel {
+	typealias Observer<T> = (T) -> Void
+	
 	private let commentsLoader: FeedImageCommentsLoader
 	var commentsLoaderTask: FeedImageCommentLoaderTask?
 	
 	init(commentsLoader: FeedImageCommentsLoader) {
 		self.commentsLoader = commentsLoader
 	}
-	
-	private enum State {
-		case pending
-		case loading
-		case canceled
-	}
-	
-	private var state = State.pending {
-		didSet {
-			onChange?(self)
-		}
-	}
-	
-	var onChange: ((FeedImageCommentsViewModel) -> Void)?
-	var onCommentsLoad: (([FeedImageComment]) -> Void)?
-	
-	var isLoading: Bool {
-		switch state {
-		case .loading:
-			return true
-		case .pending, .canceled:
-			return false
-		}
-	}
-	
+
+	var onLoadingStateChange: Observer<Bool>?
+	var onCommentsLoad: Observer<[FeedImageComment]>?
+	var onCancelTask: Observer<Void>?
+
 	func loadComments() {
-		state = .loading
+		onLoadingStateChange?(true)
 		commentsLoaderTask = commentsLoader.load { [weak self] result in
 			if let model = try? result.get() {
 				self?.onCommentsLoad?(model)
 			}
-			self?.state = .pending
+			self?.onLoadingStateChange?(false)
+
 		}
 	}
 	
 	func cancelCommentsLoaderTask() {
 		commentsLoaderTask?.cancel()
-		state = .canceled
+		onCancelTask?(())
 	}
 }
